1889185226c7e774882abfa011eed5d4b34e29a2
Jacob Winch <jacob.winch@guardian.co.uk>
2020-08-12 12:58:57 +0100

    Add a couple of unit tests

---
+++ src/test/scala/com/gu/playdeveloperapi/ConversionTest.scala
@@ -0,0 +1,23 @@
+package com.gu.playdeveloperapi
+
+import com.gu.playdeveloperapi.Conversion.Build
+import com.gu.playdeveloperapi.PlayDeveloperApi.PlayDeveloperApi.{ Release, Track }
+import org.scalatest.FunSuite
+
+class ConversionTest extends FunSuite {
+
+  val betaTrack = Track("beta", releases = List(Release("1.2.123", "completed")))
+  val productionTrack = Track("production", releases = List(Release("1.2.123", "completed")))
+  val productionTrackWithDraft = Track("production", releases = List(Release("1.2.123", "draft")))
+
+  test("searchForTrack correctly identifies a beta build") {
+    val result = Conversion.searchForTrack(List(betaTrack, productionTrack), "beta")
+    assert(result == Some(Build("1.2.123")))
+  }
+
+  test("searchForTrack correctly ignores a draft production build") {
+    val result = Conversion.searchForTrack(List(betaTrack, productionTrackWithDraft), "production")
+    assert(result == None)
+  }
+
+}
