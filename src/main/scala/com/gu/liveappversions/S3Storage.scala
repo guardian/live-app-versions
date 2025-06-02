805a80046842994eb93e2b30494a7be24892c292
Ara Cho <jarjeb@gmail.com>
2025-06-02 13:31:57 +0100

    update S3Storage.scala

--- src/main/scala/com/gu/liveappversions/S3Storage.scala
+++ src/main/scala/com/gu/liveappversions/S3Storage.scala
@@ -1,10 +1,11 @@
 package com.gu.liveappversions
 
 import java.io.ByteArrayInputStream
+import java.nio.charset.StandardCharsets
 
-import com.amazonaws.AmazonServiceException
-import com.amazonaws.services.s3.AmazonS3ClientBuilder
-import com.amazonaws.services.s3.model.{ CannedAccessControlList, ObjectMetadata, PutObjectRequest, PutObjectResult }
+import software.amazon.awssdk.services.s3.S3Client
+import software.amazon.awssdk.services.s3.model.{ ObjectCannedACL, GetObjectRequest, PutObjectRequest, PutObjectResponse, S3Exception }
+import software.amazon.awssdk.core.sync.RequestBody
 import com.gu.config.Aws
 import com.gu.config.Config.Env
 import com.gu.liveappversions.ios.Lambda.logger
@@ -16,9 +17,10 @@ import scala.util.{ Failure, Success, Try }
 
 object S3Storage {
 
-  private val s3Client = AmazonS3ClientBuilder.standard()
-    .withCredentials(Aws.credentials("developerPlayground"))
-    .withRegion(Aws.euWest1.getName)
+  private val s3Client = S3Client
+    .builder()
+    .credentialsProvider(Aws.credentials("developerPlayground"))
+    .region(Aws.euWest1)
     .build()
 
   def fullKey(env: Env, partialKey: String): String = {
@@ -26,46 +28,55 @@ object S3Storage {
     s"reserved-paths$stagePrefix$partialKey"
   }
 
-  def putJson(jsonToUpload: Json, env: Env, bucketName: String, partialKey: String): Try[PutObjectResult] = {
-
+  def putJson(jsonToUpload: Json, env: Env, bucketName: String, partialKey: String): Try[PutObjectResponse] = {
     val key = fullKey(env, partialKey)
+    val accessControl = if (env.stage == "PROD") ObjectCannedACL.PUBLIC_READ else ObjectCannedACL.PRIVATE
+    val bytes = jsonToUpload.toString().getBytes(StandardCharsets.UTF_8)
+    val stream = new ByteArrayInputStream(bytes)
 
-    val buildAttributesStream: ByteArrayInputStream = new ByteArrayInputStream(jsonToUpload.toString().getBytes)
+    val putRequest = PutObjectRequest.builder()
+      .bucket(bucketName)
+      .key(key)
+      .contentType("application/json")
+      .cacheControl("max-age=60")
+      .acl(accessControl)
+      .build()
 
-    val metadata = new ObjectMetadata()
-    metadata.setContentType("application/json")
-    metadata.setCacheControl("max-age=60")
-
-    val accessControl = if (env.stage == "PROD") {
-      CannedAccessControlList.PublicRead
-    } else {
-      CannedAccessControlList.Private //It's preferable to avoid serving test files via https://mobile.guardianapis.com/
-    }
-
-    val putObjectRequest = new PutObjectRequest(bucketName, key, buildAttributesStream, metadata)
-      .withCannedAcl(accessControl)
-
-    Try(s3Client.putObject(putObjectRequest)) match {
+    Try {
+      val requestBody = RequestBody.fromInputStream(stream, bytes.length)
+      s3Client.putObject(putRequest, requestBody)
+    } match {
       case Success(result) =>
         logger.info(s"Successfully uploaded new build info to S3 (bucket: $bucketName | key: $key)")
+        stream.close()
         Success(result)
       case Failure(exception) =>
         logger.error(s"Failed to uploaded build to S3 (bucket: $bucketName | key: $key) due to: $exception")
+        stream.close()
         Failure(exception)
     }
-
   }
 
   def getJsonString(env: Env, bucketName: String, partialKey: String): Try[Option[String]] = {
     val key = fullKey(env, partialKey)
+
+    val getRequest = GetObjectRequest.builder()
+      .bucket(bucketName)
+      .key(key)
+      .build()
+
     Try {
-      val s3Object = s3Client.getObject(bucketName, key)
-      Source.fromInputStream(s3Object.getObjectContent).mkString
+      val inputStream = s3Client.getObject(getRequest)
+      try {
+        Source.fromInputStream(inputStream).mkString
+      } finally {
+        inputStream.close()
+      }
     } match {
       case Success(result) =>
         logger.info(s"Successfully downloaded file from S3 (bucket: $bucketName | key: $key)")
         Success(Some(result))
-      case Failure(amazonServiceException: AmazonServiceException) if (amazonServiceException.getStatusCode == 404) =>
+      case Failure(s3Exception: S3Exception) if (s3Exception.statusCode() == 404) =>
         logger.info(s"File was not present in s3 (bucket: $bucketName | key: $key)")
         Success(None)
       case Failure(exception) =>
@@ -73,5 +84,4 @@ object S3Storage {
         Failure(exception)
     }
   }
-
 }
